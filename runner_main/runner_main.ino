a0c7c662592050ef8065c28a9ecf2d5cb3256a38
Myat Min Thu <mmthu.0121@gmail.com>
2020-05-09 15:47:37 +0630

    Update runner_main.ino

--- runner_main/runner_main.ino
+++ runner_main/runner_main.ino
@@ -12,20 +12,24 @@ void loop() {
     runStepperBase(100,C);      //100mm, clockwise
     Rotate = 1;
     Present = 0;
+    delay(1);
   }
 
   if (Present == 2){
     Rotate = 0;
-    runStepperBase(-100,C);
+    runStepperBase(100,A);
     Present = 0;
+    delay(1);
   }
 
   if (Rotate == 1){
-    while (distance < 30){
+    while (distance < 150){
       runStepperArm(10);
       ultrasonic_check();
+      delay(1);
     }
     Present = 2;
+    delay(1);
   }
 
 
