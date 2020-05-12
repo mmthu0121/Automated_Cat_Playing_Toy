9c508e42a1072a27af1148c7393857183a77a730
Myat Min Thu <mmthu.0121@gmail.com>
2020-05-12 16:49:10 +0630

    Update variables.h

--- runner_main/variables.h
+++ runner_main/variables.h
@@ -1,5 +1,5 @@
 // Pin and Variable Declaration
-#include <Stepper.h>
+
 
 //----------------------------LED----------------------------------------------
 #define led 11
@@ -12,13 +12,27 @@ long duration, distance;
 int Present = 0;
 int Rotate = 0;
 
+//-----------------------Motion Sensor----------------------------------------
+
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+
 //---------------------------Stepper-------------------------------------------
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+// #include <Stepper.h>
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 const int stepsPerRevolution = 200;
 
 //Stepper at the base
-Stepper baseStepper(stepsPerRevolution, 21,22,25,26);
+Adafruit_StepperMotor *baseStepper = AFMS.getStepper(200, 2);
+
+// Stepper baseStepper(stepsPerRevolution, 21,22,25,26);
 int stepCount = 0;
 
 //Stepper at the arm
-Stepper armStepper(stepsPerRevolution, 23,24,29,30);
+Adafruit_StepperMotor *armStepper = AFMS.getStepper(200, 2);
+
+// Stepper armStepper(stepsPerRevolution, 23,24,29,30);
