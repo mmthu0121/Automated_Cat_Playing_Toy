d57741d9380ccc017b109e10bdeea11207ad827d
Myat Min Thu <mmthu.0121@gmail.com>
2020-05-12 16:48:24 +0630

    Update functions.ino

--- runner_main/functions.ino
+++ runner_main/functions.ino
@@ -1,5 +1,5 @@
 
-//-------------------Ultrasonic and LED Functions----------------------------
+//-----------------------Ultrasonic Functions--------------------------------
 
 void ultrasonic_check() {
 
@@ -11,46 +11,64 @@ void ultrasonic_check() {
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
   delay(1);
-  // Serial.print(distance);
-  // Serial.println(" cm");
-  // delay (100);
 }
 
-void LED_Cat_Present_Test() {
-  if (distance > 45) {
+// void LED_Cat_Present_Test() {
+//   if (pirState > 45) {
+//     digitalWrite(led,HIGH);
+//     delay(300);
+//     digitalWrite(led, LOW);
+//     delay(300);
+//     Present = 0;
+//     delay(1);
+//   }
+//
+//   else {
+//     digitalWrite(led,LOW);
+//     Present = 1;
+//     delay(1);
+//   }
+// }
+
+//------------------------------Motion Sensor----------------------------------
+
+void MotionTest(){
+  val = digitalRead(inputPin);  // read input value
+  ultrasonic_check();
+
+  if (val == HIGH) {            // check if the input is HIGH
+    if (pirState == LOW) {
+      pirState = HIGH;
+    }
+    Present = 1;
+  } else {
+    if (pirState == HIGH){
+      pirState = LOW;
+    }
     digitalWrite(led,HIGH);
     delay(300);
     digitalWrite(led, LOW);
     delay(300);
-    Present = 0;
-    delay(1);
-  }
-
-  else {
-    digitalWrite(led,LOW);
-    Present = 1;
-    delay(1);
   }
 }
 
-//-----------------------------------------------------------------------------
 
 //-----------------------------Stepper Functions-------------------------------
-void runStepperBase (float length, char direct){
-
-  int steps = 3600*length/314;        //adjust the formula with actual hardware. 31.4 is mm and calculated from the diameter of 5mm using c = 2*pi*radius
-
-  if ( direct == 'C' ){
-    baseStepper.step(steps);
-  }else if ( direct == 'A' ){
-    baseStepper.step(-steps);
-  }
-  delay(1);
-}
-
-void runStepperArm (int motorSpeed){
-
-  armStepper.setSpeed(motorSpeed);
-  armStepper.step(50000);
-  delay(1);
-}
+// void runStepperBase (float length, char direct){
+//
+//   int steps = 3600*length/314;        //adjust the formula with actual hardware. 31.4 is mm and calculated from the diameter of 5mm using c = 2*pi*radius
+//
+//   if ( direct == 'C' ){
+//     baseStepper.step(steps);
+//   }else if ( direct == 'A' ){
+//     baseStepper.step(-steps);
+//   }
+//   delay(1);
+// }
+//
+// void runStepperArm (int motorSpeed){
+//
+//   armStepper.setSpeed(motorSpeed);
+//   armStepper.step(50000);
+//   delay(1);
+// }
