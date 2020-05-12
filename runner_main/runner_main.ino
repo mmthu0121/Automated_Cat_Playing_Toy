44aea60d88849bde0a11f1060983675849410151
Myat Min Thu <mmthu.0121@gmail.com>
2020-05-12 16:48:45 +0630

    Update runner_main.ino

--- runner_main/runner_main.ino
+++ runner_main/runner_main.ino
@@ -5,31 +5,31 @@
 
 void loop() {
 
-  ultrasonic_check();
-  LED_Cat_Present_Test();
+  MotionTest();
 
-  if (Present == 1){
-    runStepperBase(100,'C');      //100mm, clockwise
+  int length = distance - 200;
+  int steps = 3600*length/314;
+
+  if (pirState == HIGH){
+    baseStepper->step(steps, FORWARD, DOUBLE);       //100mm, clockwise
     Rotate = 1;
-    Present = 0;
     delay(1);
-  }
-
-  if (Present == 2){
-    Rotate = 0;
-    runStepperBase(100,'A');
-    Present = 0;
-    delay(1);
-  }
-
-  if (Rotate == 1){
-    while (distance < 150){
-      runStepperArm(10);
-      ultrasonic_check();
+    if (Rotate == 1){
+      while (distance < 1000 || pirState == HIGH){      //1000mm
+        armStepper->step(200, FORWARD, MICROSTEP);      //rotate one full cycle
+        MotionTest();
+        delay(1);
+      }
+      Present = 0;
       delay(1);
     }
-    Present = 2;
-    delay(1);
+
+    if (Present == 0){
+      Rotate = 0;
+      baseStepper->step(steps, BACKWARD, DOUBLE);
+      delay(1);
+      Present = 1;
+    }
   }
 
 
